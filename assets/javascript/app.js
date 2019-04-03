64330c1d95a016072a2969048e01206ec07d3544
Gregory Rutherford <gregorford@gmail.com>
2019-04-03 17:48:38 -0500

    got the dom logic working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,59 +1,96 @@
 $(this).ready();
-//display start screen with "begin" button
-//click the button, hide the start div and call the game function.
-//clicks begin
-//new question is displayed
-//store all relevant info in arrays as objects
-var correctAnswers = 0;
+
+var correctCounter = 0;
+var incorrectCounter = 0;
 var incorrectAnswers = 0;
+var count;
 var counter = 5;
+var questionIndex = 0;
 
 var questionsAnswers = [
-    {
-        question: "What is blablabla",
-        answers: ["answer 1", "answer 2", "answer3", "answer4"],
-        correctAnswer: "answer 1",
-        pic: "<img>"
-    }
-//it displays the question
-//it displays each answer as a button
-//the correct answer has a value that produces the correct screen
-//the incorrect answers have a value that produce the incorrect screen
-
-
-]
+  {
+    question: "What is ",
+    answers: ["answer 1", "answer 2", "answer3", "answer4"],
+    correctAnswer: "answer 1",
+    pic: "<img>"
+  },
+  {
+    question: "second question!",
+    answers: ["answer1", "answer2", "answer3", "answer4"],
+    correctAnswer: "answer2",
+    pic: "<img>"
+  }
+];
 startGame();
 function startGame() {
-    $("#startButton").on("click", function () {
-        $(this).hide();
-        //call countdown timer here 
-        // countDown(counter);
-        //call playGame function here
-        playGame();
-    })
-}
-
-function countDown(x) {
-    var count = setInterval(function () {
-        $("#timerFrame").html("<h2>Time Remaining: </h2>" + x, x-- ||
-            clearInterval(count))
-    }, 1000)
-}
-
-function playGame() {
+  $("#startButton").on("click", function() {
+    $(this).hide();
     countDown(counter);
-    for (var i = 0; i < questionsAnswers.length; i++) {
-        $("#questionFrame").html(questionsAnswers[i].question);
+    questionDom();
+    clickAnswer();
+  });
+}
+//timer that begins at each new question
+function countDown(x) {
+    count = setInterval(function() {
+    $("#timerFrame").html(
+      "<h2>Time Remaining: " + x + "</h2>",
+      x-- || clearInterval(count)
+    );
+  }, 1000);
+}
+// writes the question and answers to the page
+function questionDom() {
+  $("#questionFrame").text(questionsAnswers[questionIndex].question);
+  $("#answersFrame").html(
+    "<p class='answer'>" +
+      questionsAnswers[questionIndex].answers[0] +
+      "</p>" +
+      "<p class='answer'>" +
+      questionsAnswers[questionIndex].answers[1] +
+      "</p>" +
+      "<p class='answer'>" +
+      questionsAnswers[questionIndex].answers[2] +
+      "</p>" +
+      "<p class='answer'>" +
+      questionsAnswers[questionIndex].answers[3] +
+      "</p>"
+  );
+}
+//makes each answer clickable, compares the correct answers and calls the correct or incorrect dom functions
+function clickAnswer() {
+  $(".answer").click(function() {
+    console.log("you clicked me");
+    if ($(this).text() === questionsAnswers[questionIndex].correctAnswer) {
+      console.log("the correct guess has been clicked");
+      correctDom();
+      correctCounter++;
+      clearInterval(count);
+    } else {
+      incorrectDom();
+      incorrectCounter++;
+      clearInterval(count);
+      //set timeout for the next question function
     }
+  });
+}
 
+//hides the question and answer elements and displays the correct answer screen
+function correctDom() {
+  $("#questionFrame").hide();
+  $("#answersFrame").hide();
+  $("#winFrame").text("Correct");
+}
+//same as above but for the incorrect answers
+function incorrectDom() {
+    $("#questionFrame").hide();
+    $("#answersFrame").hide();
+    $("#loseFrame").text("ya guessed wrong bitch!");
 }
 
 
-//the question is displayed with 4 choices and a timer with a 30 sec countdown
-//when a choice is selected
-//the right or wrong screen is displayed,
-//while stopping the timer and displaying a cool image
-//after all the questions have been answered a new screen is displayed
-//screen contains number of correct answers and incorrect answers 
-// and a restart button
-
+// function nextQuestion(){
+//     questionIndex++;
+//     countDown();
+//     questionDom();
+// }
