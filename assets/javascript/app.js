8becc8c0acbb2d972189e14e0c23d1f6b1c0ffdd
Gregory Rutherford <gregorford@gmail.com>
2019-04-05 21:04:24 -0500

    initial styling, still need to replace a few images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ var correctCounter = 0;
 var incorrectCounter = 0;
 var incorrectAnswers = 0;
 var count;
-var counter = 5;
+var counter = 15;
 var questionIndex = 0;
 var question;
 var answers;
@@ -26,35 +26,50 @@ var questionsAnswers = [
   },
   {
     question: "What is the name of Ziggy's christmas special?",
-    answers: ["Ziggy's Gift", "Ziggy Christmas Special", "The Grinch featuring Ziggy", "Ziggy Xmas"],
+    answers: [
+      "Ziggy's Gift",
+      "Ziggy Christmas Special",
+      "The Grinch featuring Ziggy",
+      "Ziggy's Big Christmas"
+    ],
     correctAnswer: "Ziggy's Gift",
-    pic: "<img>"
+    pic: "<img src ='assets/images/gift.jpeg'>"
+  },
+  {
+    question: "What kind of pants does Ziggy wear?",
+    answers: ["Blue Jeans", "Khakis", "Courdoroys", "No Pants!"],
+    correctAnswer: "No Pants!",
+    pic: "<img src ='assets/images/pants-on-ziggy.gif'>"
+  },
+  {
+    question: "What year was the first Ziggy comic launched in?",
+    answers: ["2001", "1992", "1987", "1971"],
+    correctAnswer: "1971",
+    pic: "<img src ='assets/images/firstzig.png'>"
   }
 ];
 
-  $("#startButton").on("click", function() {
-    $(this).hide();
-    countDown(counter);
-    questionDom();
-    $("#questionFrame").show();
-    $("#answersFrame").show();
-  });
+$("#startButton").on("click", function() {
+  $(this).hide();
+  countDown(counter);
+  questionDom();
+  $("#questionFrame").show();
+  $("#answersFrame").show();
+});
 
 //timer that begins at each new question
 function countDown(x) {
-    $("#timerFrame").html(
-        "<h2>Time Remaining: " + x + "</h2>")
-    count = setInterval(function () {
-        x--;
-        if (x === -1) {
-            incorrectDom();
-            setTimeout(nextQuestion, 5000);
-            return clearInterval(count);
-        }
-        $("#timerFrame").html(
-            "<h2>Time Remaining: " + x + "</h2>",
-        );
-    }, 1000);
+  $("#timerFrame").html("<h2>Time Remaining: " + x + "</h2>");
+  count = setInterval(function() {
+    x--;
+    if (x === -1) {
+      incorrectDom();
+      incorrectCounter++;
+      setTimeout(nextQuestion, 5000);
+      return clearInterval(count);
+    }
+    $("#timerFrame").html("<h2>Time Remaining: " + x + "</h2>");
+  }, 1000);
 }
 // writes the question and answers to the page
 function questionDom() {
@@ -76,22 +91,20 @@ function questionDom() {
 }
 //makes each answer clickable, compares the correct answers and calls the correct or incorrect dom functions
 $(document).on("click", ".answer", function() {
-    console.log("you clicked me");
-    setTimeout(nextQuestion, 5000);
-    if ($(this).text() === questionsAnswers[questionIndex].correctAnswer) {
-        console.log("the correct guess has been clicked");
-        correctDom();
-        correctCounter++;
-        clearInterval(count);
-    } else {
-        console.log("the wrong guess has been clicked.");
-        incorrectDom();
-        incorrectCounter++;
-        clearInterval(count);    
-    }
+  console.log("you clicked me");
+  setTimeout(nextQuestion, 5000);
+  if ($(this).text() === questionsAnswers[questionIndex].correctAnswer) {
+    console.log("the correct guess has been clicked");
+    correctDom();
+    correctCounter++;
+    clearInterval(count);
+  } else {
+    console.log("the wrong guess has been clicked.");
+    incorrectDom();
+    incorrectCounter++;
+    clearInterval(count);
+  }
 });
-
-
 //hides the question and answer elements and displays the correct answer screen
 function correctDom() {
   $("#questionFrame").hide();
@@ -101,39 +114,46 @@ function correctDom() {
 }
 // same as above but shows the incorrect screen
 function incorrectDom() {
-    $("#questionFrame").hide();
-    $("#answersFrame").hide();
-    $("#loseFrame").show(); 
-    $("#loseFrame").html("<p> You guessed Wrong! The correct answer is: " 
-    + questionsAnswers[questionIndex].correctAnswer + "</p>"
-    + questionsAnswers[questionIndex].pic);
-  } 
+  $("#questionFrame").hide();
+  $("#answersFrame").hide();
+  $("#loseFrame").show();
+  $("#loseFrame").html(
+    "<p> You guessed wrong! The correct answer is: " +
+      questionsAnswers[questionIndex].correctAnswer +
+      "</p>" +
+      questionsAnswers[questionIndex].pic
+  );
+}
 // hides the results and moves onto the next question
 function nextQuestion() {
-    $("#winFrame").hide();
-    $("#loseFrame").hide(); 
-    questionIndex++;
-    if (questionIndex < questionsAnswers.length) {
-        countDown(counter);
-        questionDom();
-        $("#questionFrame").show();
-        $("#answersFrame").show();
-    } else {
-        $("#resultsFrame").show();
-        $("#resultsFrame").html("<h1>Thanks for playing! Here are your results: "
-        + "<br/>"  
-        + correctCounter + " correct " + "<br/>" 
-        + incorrectCounter + " incorrect </h1>");
-        setTimeout(reset, 5000); 
-    }
+  $("#winFrame").hide();
+  $("#loseFrame").hide();
+  questionIndex++;
+  if (questionIndex < questionsAnswers.length) {
+    countDown(counter);
+    questionDom();
+    $("#questionFrame").show();
+    $("#answersFrame").show();
+  } else {
+    $("#resultsFrame").show();
+    $("#resultsFrame").html(
+      "<h1>Thanks for playing! Here are your results: " +
+        "<br/>" +
+        correctCounter +
+        " correct " +
+        "<br/>" +
+        incorrectCounter +
+        " incorrect </h1>"
+    );
+    setTimeout(reset, 5000);
+  }
 }
-
-function reset () {
-    correctCounter = 0;
-    incorrectCounter = 0;
-    questionIndex = 0;
-    $("#resultsFrame").hide();
-    $("#startButton").show();
-    $("#timerFrame").hide();
-    
+//shows the start button and upon pressing the start button the game starts again
+function reset() {
+  correctCounter = 0;
+  incorrectCounter = 0;
+  questionIndex = 0;
+  $("#resultsFrame").hide();
+  $("#startButton").show();
+  $("#timerFrame").hide();
 }
